d83269a568cc71c73d0ddfdc4b542dd892096321
Matthew Dennis <dennmat@gmail.com>
2018-01-27 00:35:12 -0500

    getting tether started

---
+++ Source/tether.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export(int) var chain_count = 10
+export(Rect2) var chain_size = Rect2(5, 5)
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
